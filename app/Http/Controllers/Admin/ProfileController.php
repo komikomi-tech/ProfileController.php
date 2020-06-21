fcdc867a93a19fb5d00aba8cf2187bb2c20f4ddf
Takeru Komiya <never.say.never123da@gmail.com>
2020-06-21 02:31:17 +0000

    PHP/Laravel 12 ユーザー認証を実装する

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -13,16 +13,72 @@ class ProfileController extends Controller
         
     }
     
-    public function create(){
+    public function create(Request $request)
+    {
+         $this->validate($request, News::$rules);
+      $news = new News;
+      $form = $request->all();
+      // フォームから画像が送信されてきたら、保存して、$news->image_path に画像のパスを保存する
+      if (isset($form['image'])) {
+        $path = $request->file('image')->store('public/image');
+        $news->image_path = basename($path);
+      } else {
+          $news->image_path = null;
+      }
+      // フォームから送信されてきた_tokenを削除する
+      unset($form['_token']);
+      // フォームから送信されてきたimageを削除する
+      unset($form['image']);
+      // データベースに保存する
+      $news->fill($form);
+      $news->save();
         return redirect('admin/profile/create');
     }
     
-    public function edit(){
-        return view('admin.profile.edit');
-        
+   public function index(Request $request)
+{
+    $cond_title = $request->cond_title;
+    if ($cond_title != '') {
+        $posts = News::where('title', $cond_title)->get();
+    } else {
+        $posts = News::all();
     }
+    return view('admin.profile.index', ['posts' => $posts, 'cond_title' => $cond_title]);
+}
+
+public function edit(Request $request)
+{
+    $news = News::find($request->id);
+    if (empty($news)) {
+        abort(404);
+    }
+    return views('admin.profile.edit', ['news_form' => $news]);
+}
+
+public function update(Request $request)
+{
+    $this->validate($request, News::$rules);
+    $news = News::find($request->id);
+    $news_form = $request->all();
+    if (isset($news_form['image'])) {
+        $path = $request->file('image')->store('public/image');
+        $news->image_path = basename($path);
+        unset($news_form['image']);
+      } elseif (isset($request->remove)) {
+        $news->image_path = null;
+        unset($news_form['remove']);
+      }
+    unset($news_form['_token']);
+    $news->fill($news_form)->save();
     
-    public function update(){
-        return redirect('admin/profile/edit');
-    }
+    return redirect('admin/profile');
+}
+
+public function delete(Request $request)
+{
+    $news = News::find($request->id);
+    $news->delete();
+    return redirect('admin/profile/');
+}
+
 }
--- resources/views/admin/profile/edit.blade.php
+++ resources/views/admin/profile/edit.blade.php
@@ -11,3 +11,57 @@
         <h1>Myプロフィール修正画面</h1>
     </body>
 </html>
+
+@extends('layouts'.'profile')
+
+@section('title', 'プロフィールの編集')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 mx-auto">
+            <h2>ニュース編集</h2>
+            <form action="{{ action('Admin\ProfileController@update') }}" method="post"
+            enctype="multipart/form-date">
+                 @if (count($errors) > 0)
+                        <ul>
+                            @foreach($errors->all() as $e)
+                                <li>{{ $e }}</li>
+                            @endforeach
+                        </ul>
+                    @endif
+                    <div class="form-group row">
+                        <label class="col-md-2" for="name">氏名</label>
+                        <div class="col-md-10">
+                            <input type="text" class="form-control" name="title" value="{{ $news_form->title }}">
+                        </div>
+                    </div>
+                     <div class="form-group row">
+                    <label class="col-md-2" for="hobby">趣味</label>
+                    <div class="col-md-10">
+                        <input type="text" class="form-control" name="hobby" value="{{ old('hobby') }}">
+                        
+                        
+                    </div>
+                </div>
+                
+                
+                <div class="form-group row">
+                    <label class="col-md-2" for="introduction">自己紹介欄</label>
+                    <div class="col-md-10">
+                        <textarea class="form-control" name="introduction" rows="20">{{ old('introduction') }}</textarea>
+                    </div>
+                    
+                </div>
+                    <div class="form-group row">
+                        <div class="col-md-10">
+                            <input type="hidden" name="id" value="{{ $news_form->id }}">
+                            {{ csrf_field() }}
+                            <input type="submit" class="btn btn-primary" value="更新">
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+@endsection
